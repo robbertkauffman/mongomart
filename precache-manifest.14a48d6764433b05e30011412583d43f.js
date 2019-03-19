c932615ff56f2384cfa9668beddf71d1563d5b1e
Robbert Kauffman <robbertkauffman@gmail.com>
2019-03-19 08:31:59 -0500

    Updates

---
+++ precache-manifest.14a48d6764433b05e30011412583d43f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "430f8c32779ba9b0fed4",
+    "url": "/mongomart/static/js/main.430f8c32.chunk.js"
+  },
+  {
+    "revision": "f9e3e9dfac759db8d2a5",
+    "url": "/mongomart/static/js/runtime~main.f9e3e9df.js"
+  },
+  {
+    "revision": "f14c7552b0bb85600837",
+    "url": "/mongomart/static/js/2.f14c7552.chunk.js"
+  },
+  {
+    "revision": "dbd9f23bae061a738f693b7868fad6c7",
+    "url": "/mongomart/index.html"
+  }
+];
