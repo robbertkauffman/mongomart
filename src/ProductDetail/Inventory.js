e8dcbc46e7c6eb728ce1bf1f85eec4e2dc85b4f1
Robbert Kauffman <robbertkauffman@gmail.com>
2019-03-20 14:34:25 -0500

    improved add to cart to show visual confirmation to user and improved readability code

--- src/ProductDetail/Inventory.js
+++ src/ProductDetail/Inventory.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
 import { AnonymousCredential } from 'mongodb-stitch-browser-sdk';
 
+import Error from '../Error';
+
 export default class Inventory extends Component {
   constructor(props) {
     super(props);
     this.addToCart = this.addToCart.bind(this);
+    this.state = {
+      addToCartError: undefined
+    };
     // this.setNotification = this.setNotification.bind(this);
   }
 
   componentDidMount() {}
 
   addToCart() {
+    this.incrementProductQuantity();
+  }
+
+  incrementProductQuantity() {
     // first try to increment quantity of item in cart,
     // if fails, add item to cart or create cart (upsert)
     let incQuery = {
@@ -29,30 +38,46 @@ export default class Inventory extends Component {
         if (response && response.modifiedCount !== 1) {
           // if not incremented,
           // either add item to cart or create new cart (upsert)
-          let addQuery = { userId: this.props.client.auth.currentUser.id };
-          let addItem = this.props.item;
-          addItem.quantity = 1;
-          const addUpdate = { $addToSet: { items: addItem } };
-          const options = { upsert: true };
-
-          this.props.client.auth
-            .loginWithCredential(new AnonymousCredential())
-            .then(() =>
-              this.props.db
-                .collection('cart')
-                .updateOne(addQuery, addUpdate, options)
-            )
-            .then(response => {
-              // console.log(response);
-            })
-            .catch(err => {
-              console.error(err);
-            });
+          this.createCartOrCartItem();
+        } else {
+          this.onAddToCartSuccess();
         }
       })
       .catch(err => {
-        console.error(err);
+        this.onAddToCartError(err);
       });
+  }
+
+  createCartOrCartItem() {
+    let addQuery = { userId: this.props.client.auth.currentUser.id };
+    let addItem = this.props.item;
+    addItem.quantity = 1;
+    const addUpdate = { $addToSet: { items: addItem } };
+    const options = { upsert: true };
+
+    this.props.client.auth
+      .loginWithCredential(new AnonymousCredential())
+      .then(() =>
+        this.props.db.collection('cart').updateOne(addQuery, addUpdate, options)
+      )
+      .then(response => {
+        this.onAddToCartSuccess();
+      })
+      .catch(err => {
+        this.onAddToCartError(err);
+      });
+  }
+
+  onAddToCartSuccess() {
+    this.setState({ addToCartError: null });
+    this.refs.addToCartButton.setAttribute('disabled', '');
+    this.refs.addToCartButton.textContent = 'Added item to cart';
+    this.refs.addToCartButton.className =
+      this.refs.addToCartButton.className + ' success';
+  }
+
+  onAddToCartError(err) {
+    this.setState({ addToCartError: err });
   }
 
   setNotification() {
@@ -74,21 +99,31 @@ export default class Inventory extends Component {
   render() {
     const item = this.props.item;
     if (item.stock > 0) {
-      return (
-        <button
-          className="btn btn-primary"
-          type="submit"
-          onClick={this.addToCart}
-        >
-          Add to cart
-          <span className="glyphicon glyphicon-chevron-right" />
-        </button>
-      );
+      if (!this.state.addToCartError) {
+        return (
+          <button
+            className="btn btn-primary"
+            type="submit"
+            ref="addToCartButton"
+            onClick={this.addToCart}
+          >
+            Add to cart
+            <span className="glyphicon glyphicon-chevron-right" />
+          </button>
+        );
+      } else {
+        return (
+          <Error
+            message={'Error while adding to cart!'}
+            error={this.state.addToCartError}
+          />
+        );
+      }
     } else {
       return (
         <React.Fragment>
           <p className="red-text">Sorry, this product is out of stock</p>
-          <button
+          {/* <button
             className="btn btn-primary"
             type="submit"
             onClick={() => this.setNotification()}
@@ -96,7 +131,7 @@ export default class Inventory extends Component {
             Notify me <br />
             when in stock
             <span className="glyphicon glyphicon-bell" />
-          </button>
+          </button> */}
         </React.Fragment>
       );
     }
